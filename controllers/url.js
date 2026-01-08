c5403b4a58dc0e48684c32d16a95d8512bccbb09
Jayant <jay@Jayants-MacBook-Air.local>
2026-01-08 23:30:16 +0530

    generate url update

--- controllers/url.js
+++ controllers/url.js
@@ -7,6 +7,12 @@ async function generateNewShortUrl(req,res){
         const body=req.body;
         if(!body.url) return res.status(400).json({error:"url is required"});
         const shortId=nanoid(8);  
+        let redirect = body.url;
+
+    // 👇 If no http or https, prefix https://
+     if (!redirect.startsWith("http://") && !redirect.startsWith("https://")) {
+    redirect = "https://" + redirect;
+    }
         await URL.create({
             shortId:shortId,
             redirectUrl:body.url,
